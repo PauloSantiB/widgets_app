78e20f4dd2f1deb13e9734a8ced72abe53723f80
Paulo Benitez <paulosantibd@gmail.com>
2024-08-03 13:11:57 -0400

    screens creadas y barril actualizado

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -3,4 +3,3 @@
 export  'package:widgets_app/presentation/buttons/buttons_screens.dart';
 export  'package:widgets_app/presentation/buttons/cards/cards_screen.dart';
 export  'package:widgets_app/presentation/screens/home/home_screen.dart';
-
