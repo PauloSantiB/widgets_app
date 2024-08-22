f956c59526935931f6349ae4b67af3bcad4654cb
Paulo Benitez <paulosantibd@gmail.com>
2024-08-21 23:06:51 -0400

    fix: space added on pubspec.yaml

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
+
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+final slides = <SlideInfo>[
+  SlideInfo('Busca la comida', 'caption', 'assets/images/1.png'),
+  SlideInfo('Entrega rapida', 'caption', 'assets/images/2.png'),
+  SlideInfo('Disfruta la comida', 'caption', 'assets/images/3.png'),
+];
+
+
+
+
+
+
 class AppTutorialScreen extends StatelessWidget {
 
   static const name = 'tutorial_screen';
@@ -8,6 +28,16 @@ class AppTutorialScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          
+        ],
+      ),
+
+
+    );
   }
 }
