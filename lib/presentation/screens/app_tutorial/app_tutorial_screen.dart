ce4e674c1f6704e6421ff31b00487227c0da7df7
Paulo Benitez <paulosantibd@gmail.com>
2024-08-22 01:22:40 -0400

    imagenes agregadas

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -10,9 +10,9 @@ class SlideInfo {
 }
 
 final slides = <SlideInfo>[
-  SlideInfo('Busca la comida', 'caption', 'assets/images/1.png'),
-  SlideInfo('Entrega rapida', 'caption', 'assets/images/2.png'),
-  SlideInfo('Disfruta la comida', 'caption', 'assets/images/3.png'),
+  SlideInfo('Busca la comida', 'Occaecat ex excepteur officia in anim ad incididunt.', 'assets/images/1.png'),
+  SlideInfo('Entrega rapida', 'captiDeserunt consequat ex nostrud dolor nostrud elit excepteur minim.on', 'assets/images/2.png'),
+  SlideInfo('Disfruta la comida', 'Excepteur officia sunt occaecat est laborum laboris esse nulla in id laborum in sit.', 'assets/images/3.png'),
 ];
 
 
@@ -32,12 +32,35 @@ class AppTutorialScreen extends StatelessWidget {
 
       body: PageView(
         physics: const BouncingScrollPhysics(),
-        children: [
-          
-        ],
+        children: slides.map(
+          (slideData) => _Slide(
+            title: slideData.title, 
+           caption: slideData.caption, 
+           imageUrl: slideData.imageUrl
+          )
+        ).toList()
       ),
 
 
     );
+  }
+}
+
+class _Slide extends StatelessWidget {
+
+  final String title;
+  final String caption; 
+  final String imageUrl;
+
+  const _Slide({
+  
+    required this.title, 
+    required this.caption, 
+    required this.imageUrl
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
